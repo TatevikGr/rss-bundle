d7eca0976183c795141600dea9fa536d5a450cb2
Tatevik <tatevikg1@gmail.com>
2025-09-29 12:22:05 +0400

    register mappings

--- src/RssFeedBundle/DependencyInjection/RssFeedExtension.php
+++ src/RssFeedBundle/DependencyInjection/RssFeedExtension.php
@@ -8,12 +8,37 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-class RssFeedExtension extends Extension
+class RssFeedExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.php');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('doctrine_migrations', [
+            'migrations_paths' => [
+                'TatevikGr\\RssBundle\\RssFeedBundle\\Migrations' =>
+                    \dirname(__DIR__).'/Migrations',
+            ],
+        ]);
+
+        $container->prependExtensionConfig('doctrine', [
+            'orm' => [
+                'mappings' => [
+                    'TatevikGrRssBundle' => [
+                        'is_bundle' => false,
+                        'type' => 'attribute',
+                        'dir' => __DIR__.'/../Entity',
+                        'prefix' => 'TatevikGr\\RssBundle\\RssFeedBundle\\Entity',
+                        'alias' => 'RssBundle',
+                    ],
+                ],
+            ],
+        ]);
     }
 }
--- src/RssFeedBundle/RssFeedBundle.php
+++ src/RssFeedBundle/RssFeedBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace TatevikGr\RssBundle\RssFeedBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use TatevikGr\RssBundle\RssFeedBundle\DependencyInjection\RssFeedExtension;
 
 class RssFeedBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new RssFeedExtension();
+    }
 }
